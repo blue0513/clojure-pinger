9ec1ff128a3b072087d6364a86a34216a1057ac5
Taiju Aoki <aokitaiju0513@gmail.com>
2020-05-05 18:51:40 +0900

    something work

--- src/pinger/core.clj
+++ src/pinger/core.clj
@@ -1,6 +1,24 @@
-(ns pinger.core)
+(ns pinger.core
+  (:import (java.net URL HttpURLConnection))
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn response-code [address]
+  (let [conn ^HttpURLConnection (.openConnection (URL. address))
+        code (.getResponseCode conn)]
+    (when (< code 400)
+      (-> conn .getInputStream .close))
+    code))
+;; (response-code "http://google.com")
+
+(defn available? [address]
+  (= 200 (response-code address)))
+;; (available? "http://google.com")
+
+(defn -main []
+  (let [addresses '("http://google.com"
+                    "http://amazon.com"
+                    "http://google.com/badurl")]
+    (while true
+      (doseq [address addresses]
+        (println (available? address)))
+      (Thread/sleep (* 1000 60)))))
